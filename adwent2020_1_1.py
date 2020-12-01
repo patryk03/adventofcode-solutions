829428fdae306a01d9a7cb9688911d51f0381d28
patryk03 <patrykpiecuch3@gmail.com>
2020-12-01 23:14:42 +0100

    O(n) complexity

--- adwent2020_1_1.py
+++ adwent2020_1_1.py
@@ -3,12 +3,7 @@ with open('.\pierwszeDane.txt') as inputFile:
     for line in inputFile:
         tab.append(line)
 tab = list(map(int, tab))
-tab = sorted(tab)
 for x in tab:
-    for i in tab:
-        for n in tab:
-            if x+i+n == 2020:
-                print(x*i*n)
-                exit()
-            elif x+i+n >2020:
-                break
+    if 2020-x in set(tab):
+        print(x*(2020-x))
+        exit()
--- adwent2020_1_2.py
+++ adwent2020_1_2.py
@@ -3,12 +3,8 @@ with open('.\pierwszeDane.txt') as inputFile:
     for line in inputFile:
         tab.append(line)
 tab = list(map(int, tab))
-tab = sorted(tab)
-drugiTab = tab[::-1]
 for x in tab:
-    for i in drugiTab:
-        if x+i == 2020:
-            print(x*i)
+    for i in tab:
+        if 2020-(x+i) in set(tab):
+            print(x*i*(2020-(x+i)))
             exit()
-        elif i == x:
-            break
