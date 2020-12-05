cc1718f1285612e80f8859cf0ac7e430b40199ba
patryk03 <patrykpiecuch3@gmail.com>
2020-12-05 18:34:18 +0100

    day5

---
+++ day5/adwent2020_5.py
@@ -0,0 +1,37 @@
+def fileReader():
+    tab = []
+    with open ("piateDane.txt") as inputFile:
+        tab = inputFile.readlines()
+    return tab
+def myId(results):
+    old = results[0]
+    for x in results[1:]:
+        if x != old+1:
+            return x-1
+        old = x
+def binary(upper, moves, range, char, allMoves):
+    num = 0
+    lower = 0
+    cent = 0
+    for i in moves:
+        cent = (lower+upper)//2
+        if i == char:
+            upper = cent
+        else:
+            lower = cent+1
+            cent = lower
+        num+=1
+        if num == range:
+            if range == 3:
+                return cent
+            else:
+                otherNumber = binary(7, allMoves[7:], 3, "L", allMoves)
+                return (cent*8 + otherNumber)
+results = []
+moves = fileReader()
+for x in moves:
+    results.append(binary(127, x[:7], 7, "F", x))
+#first
+print("Max = ", max(results))
+#second
+print("My id = ", myId(sorted(results)))
